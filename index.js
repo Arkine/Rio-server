9e31c362f46778a2f148606627af95f2e74e11c8
slyngstad60 <kevan.slyngstad@geckodesigns.com>
2018-07-12 11:20:12 -0600

    added compose to handle middleware calls

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const http = require('http');
 const Server = require('./lib/app');
 
 const app = new Server();
+const testMiddleware = (req, res, next) => {
+	// console.log('Test middleware called', next);
+
+	next(null, {testData: 'test'});
+}
+
+const nextMiddleware = (req, res, next) => {
+	console.log('next middleware called');
+}
+
+app.use(testMiddleware);
+app.use(nextMiddleware);
 
 app.listen(8082, (err) => {
 	if (err) {
